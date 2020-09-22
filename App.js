521898f5b6c052e04aaf35343ed218b521c6a6d6
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-22 00:21:06 -0300

    feat: init project with React Native CLI :ok_hand:

---
+++ App.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Button,
+  FlatList,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  const addEntry = () => {
+    alert('Abrir tela de adicionar lançamento');
+  };
+
+  return (
+    <View style={{padding: 10}}>
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Saldo: $2.102,45
+      </Text>
+      <Button onPress={addEntry} title="Adicionar" />
+
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Categorias
+      </Text>
+
+      <FlatList
+        data={[
+          {key: 'Alimentação: $200'},
+          {key: 'Combustível: $12'},
+          {key: 'Aluguel: $120'},
+          {key: 'Lazer: $250'},
+          {key: 'Outros: $1200'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
+
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Ultimos Lançamentos
+      </Text>
+
+      <FlatList
+        data={[
+          {key: 'Padaria Asa Branca: $10'},
+          {key: 'Supermercado Isadora: $190'},
+          {key: 'Posto Ipiranga: $190'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
