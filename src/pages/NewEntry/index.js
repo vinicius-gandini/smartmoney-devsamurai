7c03ba314844315eeec55ce220a5e020ab02e895
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-29 11:26:18 -0300

    feat: add static props in components :ok_hand:

--- src/pages/NewEntry/index.js
+++ src/pages/NewEntry/index.js
@@ -4,9 +4,11 @@ import {View, StyleSheet, TextInput, Button} from 'react-native';
 import BalanceLabel from '../../components/BalanceLabel';
 
 const NewEntry = ({navigation}) => {
+  const currentBalance = 2064.35;
+
   return (
     <View style={styles.container}>
-      <BalanceLabel />
+      <BalanceLabel currentBalance={currentBalance} />
 
       <View>
         <TextInput style={styles.input} />
